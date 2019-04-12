894fdff8bf534dc0a217980c927766fb6b3dfe6a
David Gassner <david.gassner@gmail.com>
2019-04-12 15:14:06 -0700

    Added data binding of image resources to ImageViews

--- app/src/main/java/com/example/android/architecture/MainActivity.kt
+++ app/src/main/java/com/example/android/architecture/MainActivity.kt
@@ -1,67 +1,28 @@
 package com.example.android.architecture
 
 import android.os.Bundle
-import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.example.android.architecture.databinding.ActivityMainBinding
 import com.example.android.architecture.viewModel.DiceViewModel
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var viewmodel: DiceViewModel
-    private val imageViews by lazy {
-        arrayOf<ImageView>(die1, die2, die3, die4, die5)
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         val binding: ActivityMainBinding =
             DataBindingUtil.setContentView(this, R.layout.activity_main)
-        binding.lifecycleOwner = this
-
         setSupportActionBar(toolbar)
 
-        initViewModel()
+        binding.lifecycleOwner = this
+
+        val viewmodel = ViewModelProviders.of(this)
+            .get(DiceViewModel::class.java)
         binding.viewmodel = viewmodel
 
-    }
-
-    /**
-     * Initialize the viewmodel
-     */
-    private fun initViewModel() {
-        viewmodel = ViewModelProviders.of(this)
-            .get(DiceViewModel::class.java)
-//        viewmodel.headline.observe(this, Observer {
-//            headline.text = it
-//        })
-        viewmodel.dice.observe(this, Observer {
-            updateDisplay(it)
-        })
-    }
-
-    /**
-     * Update the display of dice
-     */
-    private fun updateDisplay(dice: IntArray?) {
-        for (i in 0 until imageViews.size) {
-            val drawableId = when (dice?.get(i)) {
-                1 -> R.drawable.die_1
-                2 -> R.drawable.die_2
-                3 -> R.drawable.die_3
-                4 -> R.drawable.die_4
-                5 -> R.drawable.die_5
-                6 -> R.drawable.die_6
-                else -> R.drawable.die_6
-            }
-            imageViews[i].setImageResource(drawableId)
-        }
     }
 
 }
--- app/src/main/java/com/example/android/architecture/viewModel/DiceViewModel.kt
+++ app/src/main/java/com/example/android/architecture/viewModel/DiceViewModel.kt
@@ -11,33 +11,72 @@ import com.example.android.architecture.R
 import com.example.android.architecture.util.DiceHelper
 
 class DiceViewModel(app: Application) : AndroidViewModel(app) {
-    val headline = MutableLiveData<String>()
-    val dice = MutableLiveData<IntArray?>()
-
     private lateinit var soundPool: SoundPool
+
+    val headline = MutableLiveData<String>()
+    val drawables = MutableLiveData<MutableMap<Int, Int>>()
 
     private var soundId = 0
     private var soundLoaded = false
-    private var context: Context
+    private var context: Context = app
 
     init {
 //      Initialize properties
-        headline.value = getApplication<Application>().getString(R.string.roll_em)
-        dice.value = intArrayOf(5, 4, 3, 2, 1)
-        context = app
+        headline.value = app.getString(R.string.roll_em)
+        drawables.value = mutableMapOf(
+            Pair(1, R.drawable.die_6),
+            Pair(2, R.drawable.die_6),
+            Pair(3, R.drawable.die_6),
+            Pair(4, R.drawable.die_6),
+            Pair(5, R.drawable.die_6)
+        )
 
-//      Set up sound pool
+//      Initialize the soundPool object
         initSoundPool()
     }
 
+    /**
+     * Called from activity_main.xml with a binding
+     */
     fun rollDice() {
-        dice.value = DiceHelper.rollDice()
-        headline.value = DiceHelper.evaluateDice(context, dice.value)
+        val rolledDice = DiceHelper.rollDice()
+        headline.value = DiceHelper.evaluateDice(context, rolledDice)
+        updateDrawables(rolledDice)
+
         if (soundLoaded) {
             soundPool.play(soundId, 1f, 1f, 0, 0, 1f)
         }
     }
 
+    /**
+     * Create a new map of die images
+     */
+    private fun updateDrawables(dice: IntArray) {
+        val newDrawables = mutableMapOf<Int, Int>()
+        for (i in 0 until dice.size) {
+            newDrawables[i + 1] = getDrawable(dice[i])
+        }
+        drawables.value = newDrawables
+    }
+
+    /**
+     * Return the die image resource for a number from 1 to 6
+     */
+    private fun getDrawable(die: Int): Int {
+        return when (die) {
+            1 -> R.drawable.die_1
+            2 -> R.drawable.die_2
+            3 -> R.drawable.die_3
+            4 -> R.drawable.die_4
+            5 -> R.drawable.die_5
+            6 -> R.drawable.die_6
+            else -> R.drawable.die_6
+        }
+    }
+
+    /**
+     * Intialize the soundpool object
+     */
     private fun initSoundPool() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             soundPool = SoundPool.Builder()
